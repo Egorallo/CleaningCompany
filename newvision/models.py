3a59adbbae45d12a851bd6263f5270333c971a8e
Egorallo <sea25042003@gmail.com>
2023-09-24 23:18:05 +0300

    images for articles & questions

--- newvision/models.py
+++ newvision/models.py
@@ -4,6 +4,7 @@ class News(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
+    image_url = models.URLField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.title
