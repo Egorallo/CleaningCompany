b55bf8ea3454453e069d5eecbe35144430898aeb
Egorallo <sea25042003@gmail.com>
2023-09-24 20:32:04 +0300

    basic pages

--- newvision/urls.py
+++ newvision/urls.py
@@ -5,5 +5,11 @@ app_name = 'newvision'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about/', views.about, name='about')
+    path('about/', views.about, name='about'),
+    path('news/', views.news, name='news'),
+    path('questions/', views.questions, name='questions'),
+    path('reviews/', views.reviews, name='reviews'),
+    path('contacts/', views.contacts, name='contacts'),
+    path('policy/', views.policy, name='policy'),
+    path('promos/', views.promos, name='promos')
 ]
--- newvision/views.py
+++ newvision/views.py
@@ -5,3 +5,21 @@ def home(request):
 
 def about(request):
     return render(request, 'newvision/about.html')
+
+def news(request):
+    return render(request, 'newvision/news.html')
+
+def questions(request):
+    return render(request, 'newvision/questions.html')
+
+def reviews(request):
+    return render(request, 'newvision/reviews.html')
+
+def contacts(request):
+    return render(request, 'newvision/contacts.html')
+
+def policy(request):
+    return render(request, 'newvision/policy.html')
+
+def promos(requset):
+    return render(requset, 'newvision/promos.html')
