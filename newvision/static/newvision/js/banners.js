fe48efe96eaf4d204ebbe0162eb0924300d3c1fe
Egorallo <sea25042003@gmail.com>
2023-11-11 00:24:25 +0300

    banners done

--- newvision/static/newvision/js/banners.js
+++ newvision/static/newvision/js/banners.js
@@ -1,23 +1,43 @@
-document.addEventListener("DOMContentLoaded", function () {
-    var bannerDataElements = document.querySelectorAll(".banner-data");
-    var currentBannerIndex = 0;
+var bannerDataElements = document.querySelectorAll(".banner-data");
+var currentBannerIndex = 0;
+var rotationInterval = 3000;
+var rotationTimer;
 
-    function showBanner() {
-        var bannerData = bannerDataElements[currentBannerIndex];
-        var linkUrl = bannerData.getAttribute("data-link-url");
-        var imageUrl = bannerData.getAttribute("data-image-url");
-
-        document.getElementById("banner-link").href = linkUrl;
-        document.getElementById("banner-image").src = imageUrl;
-    }
-
-    function rotateBanners() {
-        showBanner();
-        setInterval(function () {
-            currentBannerIndex = (currentBannerIndex + 1) % bannerDataElements.length;
-            showBanner();
-        }, 3000);
-    }
-
+window.addEventListener('focus', function () {
+    clearInterval(rotationTimer);
     rotateBanners();
-})
+});
+
+window.addEventListener('blur', function () {
+    clearInterval(rotationTimer);
+});
+
+function showBanner() {
+    var bannerData = bannerDataElements[currentBannerIndex];
+    var linkUrl = bannerData.getAttribute("data-link-url");
+    var imageUrl = bannerData.getAttribute("data-image-url");
+
+    document.getElementById("banner-link").href = linkUrl;
+    document.getElementById("banner-image").src = imageUrl;
+}
+
+function rotateBanners() {
+    showBanner();
+    rotationTimer = setInterval(function () {
+        currentBannerIndex = (currentBannerIndex + 1) % bannerDataElements.length;
+        showBanner();
+    }, rotationInterval);
+}
+
+function updateRotationInterval() {
+    var newInterval = parseInt(document.getElementById("rotation-interval").value) * 1000; // Convert to milliseconds
+    if (!isNaN(newInterval) && newInterval > 0) {
+        rotationInterval = newInterval;
+        clearInterval(rotationTimer);
+        rotateBanners();
+    }
+}
+
+rotateBanners();
+
+document.getElementById("update-interval-button").addEventListener("click", updateRotationInterval);
