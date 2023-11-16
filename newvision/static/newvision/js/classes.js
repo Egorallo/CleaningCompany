bb88085d55531e92b3b267c76d888c6ecda8a597
Egorallo <sea25042003@gmail.com>
2023-11-16 15:33:49 +0300

    task with classes done

--- newvision/static/newvision/js/classes.js
+++ newvision/static/newvision/js/classes.js
@@ -1,3 +1,91 @@
+// function Equipment(brand, condition, price, inStock) {
+//     this._brand = brand;
+//     this._condition = condition;
+//     this._price = price;
+//     this._inStock = inStock;
+// }
+//
+// Equipment.prototype.brand = function (newBrand) {
+//     if (newBrand) this._brand = newBrand;
+//     return this._brand;
+// };
+//
+// Equipment.prototype.condition = function (newCondition) {
+//     if (newCondition) this._condition = newCondition;
+//     return this._condition;
+// };
+//
+// Equipment.prototype.equipmentInfo = function () {
+//     alert('Brand: ' + this._brand + ' Condition: ' + this._condition + ' Price: ' + this._price + ' In stock: ' + this._inStock);
+// };
+//
+// Equipment.prototype.updatePrice = function (newPrice) {
+//     this._price = newPrice;
+//     alert('Price has been updated: ' + this._price);
+// };
+//
+// Equipment.prototype.inStock = function (newInStock) {
+//     if (newInStock) this._inStock = newInStock;
+//     return this._inStock;
+// };
+//
+// Equipment.prototype.calculateTotalValue = function () {
+//     return this._price * this._inStock;
+// };
+//
+// function PressureWasher(brand, condition, price, inStock, power, isHandheld) {
+//     Equipment.call(this, brand, condition, price, inStock);
+//     this._power = power;
+//     this._isHandheld = isHandheld;
+// }
+//
+// PressureWasher.prototype = Object.create(Equipment.prototype);
+//
+// PressureWasher.prototype.power = function (newPower) {
+//     if (newPower < 100) {
+//         alert("Invalid power parameter");
+//     } else {
+//         this._power = newPower;
+//     }
+//     return this._power;
+// };
+//
+// PressureWasher.prototype.isHandheld = function (newIsHandheld) {
+//     if (newIsHandheld !== undefined) this._isHandheld = newIsHandheld;
+//     return this._isHandheld;
+// };
+//
+// PressureWasher.prototype.discountPrice = function (discountPercentage) {
+//     if (discountPercentage >= 0 && discountPercentage <= 100) {
+//         return this._price - (this._price * (discountPercentage / 100));
+//     } else {
+//         alert("Invalid discount percentage");
+//         return null;
+//     }
+// };
+//
+// PressureWasher.prototype.powerUp = function (increase) {
+//     const newPower = this._power + increase;
+//     if (newPower < 100) {
+//         alert("Invalid power parameter");
+//     } else {
+//         this._power = newPower;
+//         alert(`Power increased to: ${this._power}`);
+//     }
+// };
+
+
+function hugeValue(func) {
+    return function () {
+        const result = func.apply(this);
+        if (result >= 2000) {
+            alert('Huge value of equipment in stock: $' + result)
+        } else {
+            alert("Low baller..")
+        }
+    };
+}
+
 class Equipment {
     constructor(brand, condition, price, inStock) {
         this._brand = brand;
@@ -38,6 +126,10 @@ class Equipment {
     get inStock() {
         return this._inStock;
     }
+
+    calculateTotalValue() {
+        return this._price * this._inStock;
+    }
 }
 
 class PressureWasher extends Equipment {
@@ -67,7 +159,7 @@ class PressureWasher extends Equipment {
         return this._isHandheld;
     }
 
-     discountPrice(discountPercentage) {
+    discountPrice(discountPercentage) {
         if (discountPercentage >= 0 && discountPercentage <= 100) {
             return this.price - (this.price * (discountPercentage / 100));
         } else {
@@ -89,6 +181,9 @@ class PressureWasher extends Equipment {
 
 const pressureWasher = new PressureWasher('Karcher', 'Good', 200, 10, '1200', true);
 
+Equipment.prototype.calculateTotalValue = hugeValue(Equipment.prototype.calculateTotalValue);
+
+pressureWasher.calculateTotalValue();
 console.log(pressureWasher.power);
 console.log(pressureWasher.isHandheld);
 
